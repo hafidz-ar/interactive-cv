a7c1b090b3a36d1de1aac2094db341df58e9c5a5
Hafidz <hafidzar93@gmail.com>
2025-07-12 00:56:02 +0700

    chore: setup Vercel deployment

--- backend/data.js
+++ backend/data.js
@@ -11,7 +11,7 @@ const skills = [
 ];
 
 const projects = [ 
-    { title: 'Website Topupin', image: '/frontend/src/assets/foto/Topupinn.png',
+    { title: 'Website Topupin', image: '/src/assets/foto/Topupinn.png',
 description: 'Platform Top-up game online.', tech: ['Tailwind', 'Laravel',
 'MySql'], link: '#' }
 ];
