c20761ce75efe1dc339023249c380474c1278842
Hafidz <hafidzar93@gmail.com>
2025-07-11 23:40:42 +0700

    chore: setup Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,3 @@ app.get('/api/projects', (req, res) => res.json(projects));
 app.listen(PORT, () => {
 console.log(` Server backend berjalan di http://localhost:${PORT}`);
 });
-
-module.exports = app;
